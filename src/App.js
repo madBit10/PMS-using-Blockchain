953187d385157733bc9b05d021b050cf21907854
sanjayjogi <s.jogi@somaiya.edu>
2022-09-21 23:40:22 +0530

    ok

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { db } from "./firebase";
+import { ref, onValue } from "firebase/database";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  //read
+  useEffect(() => {
+    onValue(ref(db), (snapshot) => {
+      const data = snapshot.val();
+      setData({
+        Humidity: data.Humidity,
+        Temperature : data.Temperature,
+        Moisture: data.Moisture,
+        Pump: data.Pump
+      })
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Humidity: {data.Humidity}</h1>
+      <h1>Temperature: {data.Temperature}</h1>
+      <h1>Moisture: {data.Moisture}</h1>
+      <h1>Pump: {data.Pump}</h1>
+    </div>
+  );
+}
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDn0swCiZFn4p5hy-DTZlS-Rf2bPKT1Cfk",
+  authDomain: "node-3b506.firebaseapp.com",
+  databaseURL: "https://node-3b506-default-rtdb.firebaseio.com",
+  projectId: "node-3b506",
+  storageBucket: "node-3b506.appspot.com",
+  messagingSenderId: "679406939501",
+  appId: "1:679406939501:web:1ee456d36a2a8247cb3e64",
+  measurementId: "G-VRDEXMCJCL"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
