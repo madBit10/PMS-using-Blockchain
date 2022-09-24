20b03936e6523a7632b6a9be7701c974876998ca
dhruvsolanki0811 <dhruv.ds@somaiya.edu>
2022-09-24 19:47:14 +0530

    Update DataCards.jsx

--- src/components/DataCards.jsx
+++ src/components/DataCards.jsx
@@ -1,13 +1,52 @@
 import Card from "react-bootstrap/Card";
 import DataIcons from "./icons/DataIcons";
+
 const DataCards = ({ type , data }) => {
+  let parameters;
+  let color;
+  let units;
+  if(data==1){
+    data="On"
+  }else if(data == 0){
+    data="Off"
+  }
+  if (type=="water" ){
+    parameters="Moisture";
+    color="#cbd7ff";
+    units="_"
+
+  }
+  else if (type=="pump"){
+    parameters="pump";
+    color="#F5AA07";
+    units="_"
+  }
+  else if (type=="temp"){
+    parameters="temperature";
+    color="#b77f53";
+    units="ºC"
+  }
+  else if (type=="humid"){
+    parameters="Humidity";
+    color="#58896b";
+    units="%"
+  }
   return (
+   
     <>
       <Card className="mt-3">
         <div className="p-3">
           <DataIcons type={`${type}`} />
-          <h1>{data}</h1>
+          <br/>
+          <h5 style={{ color: `${color}` }}>{parameters}</h5>
+
+          <br/>
+          <h1 >{data}</h1>
+
+          <div id="units"  style={{ color: `${color}` }}>{units}</div>
         </div>
+
+
       </Card>
     </>
   );
