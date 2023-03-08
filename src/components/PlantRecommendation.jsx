c920b6e0e5ecf1152c138ebfc7205698058aff64
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2023-03-08 17:37:55 +0530

    made form pretty

--- src/components/PlantRecommendation.jsx
+++ src/components/PlantRecommendation.jsx
@@ -1,104 +1,164 @@
 import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Row, Col } from 'react-bootstrap'
 
 const PlantRecommendation = () => {
   const [crop, setCrop] = useState()
-  
+
   return (
     <Card
       className="mx-auto mt-6 card-ab scale-up-center"
       style={{ width: '80%' }}
     >
-        <div>
-     <Formik
-       initialValues={{
-        N: 0,
-        P: 0,
-        K: 0,
-        temp: 0,
-        humidity: 0,
-        ph:0,
-        rainfall:0,
-        model: 'svm'
-       }}
-      //  validate={values => {
-      //    const errors = {};
-      //    if (!values.email) {
-      //      errors.email = 'Required';
-      //    } else if (
-      //      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-      //    ) {
-      //      errors.email = 'Invalid email address';
-      //    }
-      //    return errors;
-      //  }}
-       onSubmit={async(values, { setSubmitting }) => {
-        
+      <Row>
+        <Formik
+          initialValues={{
+            N: 0,
+            P: 0,
+            K: 0,
+            temp: 0,
+            humidity: 0,
+            ph: 0,
+            rainfall: 0,
+            model: 'svm',
+          }}
+          //  validate={values => {
+          //    const errors = {};
+          //    if (!values.email) {
+          //      errors.email = 'Required';
+          //    } else if (
+          //      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          //    ) {
+          //      errors.email = 'Invalid email address';
+          //    }
+          //    return errors;
+          //  }}
+          onSubmit={async (values, { setSubmitting }) => {
+            const okay = await fetch(
+              `http://127.0.0.1:5000/predict?model=${values.model}&n=${values.N}&p=${values.P}&k=${values.K}&temperature=${values.temp}&humidity=${values.humidity}&ph=${values.ph}&rainfall=${values.rainfall}`
+            )
+            const kk = await okay.json()
+            console.log(kk)
+            setCrop(kk)
+            setSubmitting(false)
 
-           const okay=await fetch(`http://127.0.0.1:5000/predict?model=${values.model}&n=${values.N}&p=${values.P}&k=${values.K}&temperature=${values.temp}&humidity=${values.humidity}&ph=${values.ph}&rainfall=${values.rainfall}`  )
-           const kk= await okay.json()
-           console.log(kk) 
-           setCrop(kk)
-           setSubmitting(false);
+            //  setTimeout(() => {
+            //   //  alert(JSON.stringify(values, null, 2));
 
-      
-        //  setTimeout(() => {
-        //   //  alert(JSON.stringify(values, null, 2));
-
-        //    console.log(values)
-        //    setSubmitting(false);
-        //  }, 400);
-       }}
-     >
-       {({ isSubmitting }) => (
-         <Form>
-          <div className="form-group row">
-          <label className="col-form-label">N</label>
-           <Field type="number" name="N" />
-          </div>
-           {/* <ErrorMessage name="email" component="div" /> */}
-           <div className="form-group row">
-           <label className="col-form-label">P</label>
-           <Field type="number" name="P" />
-           </div>
-           <div className="form-group row">
-           <label className="col-form-label">K</label>
-           <Field type="number" name="K" />
-          </div>
-          <div className="form-group row">
-          <label className="col-form-label">Temp</label>
-           <Field type="number" name="temp" />
-          </div>
-          <div className="form-group row">
-          <label className="col-form-label">Humidity</label>
-           <Field type="number" name="humidity" />
-          </div>
-          <div className="form-group row">
-          <label className="col-form-label">ph</label>
-           <Field type="number" name="ph" />
-          </div>
-          <div className="form-group row">
-          <label className="col-form-label">rainfall</label>
-           <Field type="number" name="rainfall" />
-          </div>
-          <div className="form-group row">
-          <label className="col-form-label">model</label>
-          <Field as="select" name="model">
-             <option value="rf">Random Forest</option>
-             <option value="dt">Deceision tree</option>
-             <option value="svm">SVM</option>
-           </Field>
-          </div>
-           {/* <ErrorMessage name="password" component="div" /> */}
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </Form>
-       )}
-     </Formik>
-   </div>
-   {crop && <div>{crop.crop}</div>}
+            //    console.log(values)
+            //    setSubmitting(false);
+            //  }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className='slide-in-left'>
+              <Row>
+                <Col>
+                  <div className="form-group row p-2">
+                    <label className="col-form-label">N</label>
+                    <Field
+                      type="number"
+                      className="form-control input-bg"
+                      name="N"
+                    />
+                  </div>
+                </Col>
+                {/* <ErrorMessage name="email" component="div" /> */}
+                <Col>
+                  <div className="form-group row p-2">
+                    <label className="col-form-label">P</label>
+                    <Field
+                      type="number"
+                      className="form-control input-bg"
+                      name="P"
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="form-group row p-2">
+                    <label className="col-form-label">K</label>
+                    <Field
+                      type="number"
+                      className="form-control input-bg"
+                      name="K"
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="form-group row p-2">
+                    <label className="col-form-label">Temp</label>
+                    <Field
+                      type="number"
+                      className="form-control input-bg"
+                      name="temp"
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="form-group row p-2">
+                    <label className="col-form-label">Humidity</label>
+                    <Field
+                      type="number"
+                      className="form-control input-bg"
+                      name="humidity"
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="form-group row p-2">
+                    <label className="col-form-label">ph</label>
+                    <Field
+                      type="number"
+                      className="form-control input-bg"
+                      name="ph"
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="form-group row p-2">
+                    <label className="col-form-label">rainfall</label>
+                    <Field
+                      type="number"
+                      className="form-control input-bg"
+                      name="rainfall"
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <div className="form-group row p-2">
+                    <label className="col-form-label">model</label>
+                    <Field
+                      as="select"
+                      className="form-select input-bg"
+                      name="model"
+                    >
+                      <option value="rf">Random Forest</option>
+                      <option value="dt">Deceision tree</option>
+                      <option value="svm">SVM</option>
+                    </Field>
+                  </div>
+                </Col>
+              </Row>
+              {/* <ErrorMessage name="password" component="div" /> */}
+              <div className="mx-auto pt-1 pb-1">
+                <center>
+              <button type="submit" className="p-butt" disabled={isSubmitting}>
+                Submit
+              </button>
+                </center>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </Row>
+      {crop && <div>{crop.crop}</div>}
     </Card>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { useState } from "react";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import Nav from "react-bootstrap/Nav";
-import Icon from "./icons/Icon";
-import Col from "react-bootstrap/Col";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { TiHome } from "react-icons/ti";
-import { BsInfoSquareFill } from "react-icons/bs";
-import { ImStatsDots } from "react-icons/im";
-import { FaDisease } from "react-icons/fa";
-
+import { useState } from 'react'
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import Nav from 'react-bootstrap/Nav'
+import Icon from './icons/Icon'
+import Col from 'react-bootstrap/Col'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { TiHome } from 'react-icons/ti'
+import { BsInfoSquareFill } from 'react-icons/bs'
+import { ImStatsDots } from 'react-icons/im'
+import { FaDisease } from 'react-icons/fa'
+import { MdRecommend } from 'react-icons/md'
 
 function Sidebar() {
   //offcanvas states
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
 
   //Style variables
-  
+
   const background = {
-    backgroundColor: "rgba(244,248,249,255)",
-  };
+    backgroundColor: 'rgba(244,248,249,255)',
+  }
 
   return (
     <>
@@ -33,33 +33,40 @@ function Sidebar() {
       </Col>
       <Col xs={2} md={1} className="text-color-primary">
         <h1>
-          <NavLink  activeClassName='active-link' to='/'>
+          <NavLink activeClassName="active-link" to="/">
             <TiHome className="nav-icons" />
           </NavLink>
         </h1>
       </Col>
       <Col xs={2} md={1} className="text-color-primary">
         <h1>
-          <NavLink to='/aboutus'>
-          <BsInfoSquareFill className="nav-icons" />
+          <NavLink to="/aboutus">
+            <BsInfoSquareFill className="nav-icons" />
           </NavLink>
         </h1>
       </Col>
       <Col xs={2} md={1} className="text-color-primary">
         <h1>
-          <NavLink to='/stats'>
+          <NavLink to="/stats">
             <ImStatsDots className="nav-icons" />
           </NavLink>
         </h1>
       </Col>
       <Col xs={2} md={1} className="text-color-primary">
         <h1>
-          <NavLink to='/detection'>
+          <NavLink to="/detection">
             <FaDisease className="nav-icons" />
           </NavLink>
         </h1>
       </Col>
-      <Offcanvas show={show} onHide={handleClose} style={{ width: "200px" }}>
+      <Col xs={2} md={1} className="text-color-primary">
+        <h1>
+          <NavLink to="/recommendation">
+            <MdRecommend className="nav-icons" />
+          </NavLink>
+        </h1>
+      </Col>
+      <Offcanvas show={show} onHide={handleClose} style={{ width: '200px' }}>
         <Offcanvas.Header closeButton style={background}>
           <Offcanvas.Title>
             <Icon icon_name="logo.jpg" h="5rem" w="5rem" />
@@ -67,23 +74,31 @@ function Sidebar() {
         </Offcanvas.Header>
         <Offcanvas.Body className="side">
           <Nav defaultActiveKey="/home" className="flex-column">
-          <NavLink  activeClassName='active-link' to='/'>
-            <Nav.Link className="link" href="/">Home</Nav.Link>
-          </NavLink>
-          <NavLink  activeClassName='active-link' to='/aboutus'>
-            <Nav.Link className="link" href="/aboutus">About</Nav.Link>
-          </NavLink>
-          <NavLink  activeClassName='active-link' to='/stats'>
-            <Nav.Link className="link" href="/stats">Stats</Nav.Link>
-          </NavLink>
-          <NavLink  activeClassName='active-link' to='/detection'>
-            <Nav.Link className="link" href="/detection">Detecion</Nav.Link>
-          </NavLink>
+            <NavLink activeClassName="active-link" to="/">
+              <Nav.Link className="link" href="/">
+                Home
+              </Nav.Link>
+            </NavLink>
+            <NavLink activeClassName="active-link" to="/aboutus">
+              <Nav.Link className="link" href="/aboutus">
+                About
+              </Nav.Link>
+            </NavLink>
+            <NavLink activeClassName="active-link" to="/stats">
+              <Nav.Link className="link" href="/stats">
+                Stats
+              </Nav.Link>
+            </NavLink>
+            <NavLink activeClassName="active-link" to="/detection">
+              <Nav.Link className="link" href="/detection">
+                Detecion
+              </Nav.Link>
+            </NavLink>
           </Nav>
         </Offcanvas.Body>
       </Offcanvas>
     </>
-  );
+  )
 }
 
-export default Sidebar;
+export default Sidebar
